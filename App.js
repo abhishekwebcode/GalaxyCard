8fa0081cc8b2eed21ff65a0bc609da07ff6a8e73
Abhishek Mathur(abhishekwebcode) <mathur17021play@gmail.com>
2021-05-16 12:33:06 +0530

    Added expo in bare mode

--- App.js
+++ App.js
@@ -8,24 +8,23 @@
 import React, {useEffect, useState} from 'react';
 import {Button, StyleSheet, Text, View,} from 'react-native';
 import {Camera, useCameraDevices} from 'react-native-vision-camera';
-
-
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="Result" component={showResult}/>
+                <Stack.Screen name="FirstScreen" component={CameraComponent}/>
+                <Stack.Screen name="SecondScreen" component={ResultComponent}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
 };
+
+
 
 const CameraComponentStyles = StyleSheet.create({
     screen: {
@@ -53,6 +52,9 @@ const CameraComponentStyles = StyleSheet.create({
 class CameraComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.navigation=props.navigation;
+    
+        console.log(`INPUT`,Object.keys(props));
         this.camera = React.createRef();
         this.device = null;
         this.state = {
@@ -98,9 +100,8 @@ class CameraComponent extends React.Component {
     }
 
     async takePhoto() {
-        console.log(`TAKING`);
-        const photo = await this.camera.current.takePhoto({flash: 'on'});
-        console.log(photo.path)
+        const photo = await this.camera.current.takePhoto({flash: 'off'});
+        this.navigation.navigate("SecondScreen",{photoObject:photo});
     }
 
     cameraStarted() {
@@ -141,7 +142,37 @@ class CameraComponent extends React.Component {
     }
 }
 
+class ResultComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.navigation=props.navigation;
+        this.route=props.route;
+        this.photoObject = this.route.params.photoObject;
+        console.log(this.photoObject);
+        this.state={};
+    }
+    render() {
+        return <Text>Detecting Faces and Loading....</Text>
+    }
+}
 
-export default CameraComponent;
 
-const takenPhoto = {"height": 480, "isRawPhoto": false, "metadata": {"Orientation": 6, "{Exif}": {"ApertureValue": 0, "BrightnessValue": 0, "ColorSpace": 65535, "DateTimeDigitized": "2021:05:16 11:20:34", "DateTimeOriginal": null, "ExifVersion": "0210", "ExposureBiasValue": 0, "ExposureMode": 0, "ExposureProgram": 0, "ExposureTime": 0.01, "FNumber": 2.8, "Flash": 0, "FocalLenIn35mmFilm": 0, "FocalLength": 5, "ISOSpeedRatings": [Array], "LensMake": null, "LensModel": null, "LensSpecification": [Array], "MeteringMode": 0, "OffsetTime": null, "OffsetTimeDigitized": null, "OffsetTimeOriginal": null, "PixelXDimension": 640, "PixelYDimension": 480, "SceneType": 1, "SensingMethod": 1, "ShutterSpeedValue": 0, "SubjectArea": [Array], "SubsecTimeDigitized": "0", "SubsecTimeOriginal": "0", "WhiteBalance": 0}, "{TIFF}": {"DateTime": "2021:05:16 11:20:34", "Make": "Google", "Model": "sdk_gphone_x86", "ResolutionUnit": 2, "Software": null, "XResolution": 72, "YResolution": 72}}, "path": "/data/user/0/com.galaxycard/cache/mrousavy4579955857631926306.jpg", "width": 640}
+export default App;
+
+const takenPhoto = {
+    "height": 480, 
+    "isRawPhoto": false,
+     "metadata": {"Orientation": 6, "{Exif}":
+      {"ApertureValue": 0, "BrightnessValue": 0, "ColorSpace": 65535,
+       "DateTimeDigitized": "2021:05:16 11:20:34", "DateTimeOriginal": null, 
+       "ExifVersion": "0210", "ExposureBiasValue": 0, "ExposureMode": 0, "ExposureProgram": 0,
+        "ExposureTime": 0.01, "FNumber": 2.8, "Flash": 0, "FocalLenIn35mmFilm": 0, "FocalLength": 5,
+         "ISOSpeedRatings": [Array], "LensMake": null, "LensModel": null, "LensSpecification": [Array],
+          "MeteringMode": 0, "OffsetTime": null, "OffsetTimeDigitized": null, "OffsetTimeOriginal": null,
+           "PixelXDimension": 640, "PixelYDimension": 480, "SceneType": 1, "SensingMethod": 1, "ShutterSpeedValue": 0, 
+           "SubjectArea": [Array], "SubsecTimeDigitized": "0", "SubsecTimeOriginal": "0", "WhiteBalance": 0}, "{TIFF}": 
+           {"DateTime": "2021:05:16 11:20:34", "Make": "Google", "Model": "sdk_gphone_x86", "ResolutionUnit": 2, 
+           "Software": null, "XResolution": 72, "YResolution": 72}},
+            "path": "/data/user/0/com.galaxycard/cache/mrousavy4579955857631926306.jpg",
+             "width": 640
+}
